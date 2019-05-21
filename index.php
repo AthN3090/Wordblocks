0b177ff56ea6e60085351f133a51688339a7a8ed
AthN3090 <amandevrishi12@gmail.com>
2019-05-21 17:12:55 +0530

    small change

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
+
 <?php include('server.php') ?>
+
 <html>
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+    <head>
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    
     <link rel="icon" href="favicon.ico" type="image/x-icon" />
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
     <script src="myScript.js"></script>
-    <title>
-    Welcome to BlogsPot !
-    </title>
-<link rel="stylesheet" type="text/css" href="css/myCss.css"> <!-- linked myCss file -->
-<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    
+    <!-- TITLE -->    
+        <title>
+    
+        Welcome to BlogsPot !
+    
+        </title>
+
+    <link rel="stylesheet" type="text/css" href="css/myCss.css"> <!-- linked myCss file -->
+
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+   
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     
     </head>
 
